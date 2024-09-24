fb046a2d32a2636b72e80c2308ef3b7b449c33c1
Boucau Caroline <43622312+Golloumette@users.noreply.github.com>
2024-09-24 13:53:45 +0200

    template et accueil (#4)
    Co-authored-by: Alexis-Dutoit <dev.alexis14@gmail.com>

--- views/accueil.php
+++ views/accueil.php
@@ -1,44 +1,27 @@
-<!DOCTYPE html>
+<?php ob_start(); ?>
+  <section class = "talent">
 
-<html lang="fr">
-  <head>
-    <title>Les farfadet on du talent</title>
-    <link rel="icon" href="">
-    <link href="css/accueil.css" rel="stylesheet"/>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.0/mdb.min.css" rel="stylesheet"/>
-  </head>
+    <img class="photo_profil" src="../Images/BDE.png" />
+    <div class="description">
+ 
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla porta velit a sodales vestibulum. Vestibulum
+        interdum, massa in sodales auctor, lorem nibh feugiat ante, ut placerat ante ex non ipsum. Nulla non scelerisque
+        purus, id rutrum ipsum. Proin eu ex elementum, vulputate orci id, gravida tortor. Maecenas congue blandit
+        interdum. Suspendisse eget enim eu est finibus tempus. Donec dignissim in lectus eget pretium. Morbi interdum
+        aliquet magna, in ultrices quam facilisis et. Sed elementum vitae lectus vitae porta. Praesent ut ex viverra dui
+        tempus volutpat et nec lectus. In ut ipsum porttitor, aliquet lectus in, placerat leo. Morbi lobortis ultricies
+        leo non semper.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla porta velit a sodales vestibulum. Vestibulum
+        interdum, massa in sodales auctor, lorem nibh feugiat ante, ut placerat ante ex non ipsum. Nulla non scelerisque
+        purus, id rutrum ipsum. Proin eu ex elementum, vulputate orci id, gravida tortor. Maecenas congue blandit
+        interdum. Suspendisse eget enim eu est finibus tempus. Donec dignissim in lectus eget pretium. Morbi interdum
+        aliquet magna, in ultrices quam facilisis et. Sed elementum vitae lectus vitae porta. Praesent ut ex viverra dui
+        tempus volutpat et nec lectus. In ut ipsum porttitor, aliquet lectus in, placerat leo. Morbi lobortis ultricies
+        leo non semper.</p>
+    </div>
 
-  <body>
-    <header> 
-      <img class="image epsi " src=""/>
-      <img class="image fartadet " src=""/>
-      <img class="image wis " src=""/>
-    </header>
+  </section>
 
-    <section>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary navbar-static-top">
-        <div class="container-fluid">
-          <a class="navbar-brand activeMenu tab active-tab" href="accueil.html">Accueil</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">        
-              <li class="activeMenu tab"><a class="nav-link" aria-current="page" href="./propos.html">Nos secteur d'activités</a></li>
-              <li class="activeMenu tab"><a class="nav-link" aria-current="page" href="./polyculture.html">Focus polyculture</a></li>
-              <li class="activeMenu tab"><a class="nav-link" aria-current="page" href="./pensions.html">Focus pensions équidés</a></li>
-              <li class="activeMenu tab"><a class="nav-link" aria-current="page" href="./actualite.html">Nos actualités</a></li>
-              <li class="activeMenu tab"><a class="nav-link" aria-current="page" href="./profil.html">Mon compte</a></li>
-            </ul>
-            <button type="button" class="btn btn-success tab" id="idBtnS"><a class="idBtn" href="./identifier.html">
-              S'identifier</a></button>
-          </div>
-        </div>
-      </nav>
-    </section>
-      
+  <?php $content = ob_get_clean(); ?>
 
-  </body>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-</html>
+<?php require('template.php') ?>
---
+++ views/template.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+
+<head>
+  <title>Les farfadet on du talent</title>
+  <link rel="icon" href="/Images/BDE.png">
+  <link href="../css/accueil.css" rel="stylesheet" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.0/mdb.min.css" rel="stylesheet" />
+</head>
+
+<body>
+  <header class="image-container">
+    <img class="image-epsi" src="../Images/EPSI.png" />
+    <img class="image-fartadet " src="../Images/BDE.png" />
+    <img class="image-wis " src="../Images/WIS.png" />
+  </header>
+
+  <section class = "navigation">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary navbar-static-top">
+      <div class="container-fluid">
+        <a class="navbar-brand activeMenu tab active-tab" href="accueil.php">Accueil</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="activeMenu tab"><a class="nav-link" aria-current="page" href="./propos.html">btn1</a></li>
+            <li class="activeMenu tab"><a class="nav-link" aria-current="page" href="./polyculture.html">btn2</a></li>
+            <li class="activeMenu tab"><a class="nav-link" aria-current="page" href="./pensions.html">btn3</a></li>
+          </ul>
+          <button type="button" class="btn btn-success tab" id="idBtnS"><a class="idBtn" href="./identifier.html">
+              S'identifier</a></button>
+        </div>
+      </div>
+    </nav>
+  </section>
+
+  <?= $content ?>
+
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
+</html>
