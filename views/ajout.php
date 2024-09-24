b870207210c5adf08722a03069b78adad545b9d8
RomainVandenbilcke <romain.vandenbilcke@gmail.Com>
2024-09-24 14:03:05 +0200

    ajout

--- views/ajout.php
+++ views/ajout.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ajout de présentation</title>
-
-</head>
+<?php ob_start(); ?>
 <body>
 
                     <h2>Ajout de présentation</h2>
@@ -75,4 +67,7 @@
         </div>
      
 </body>
-</html>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php') ?>
