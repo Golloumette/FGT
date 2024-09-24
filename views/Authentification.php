0561f2ac2c9576786c0e0b6626dc4798f002512a
RomainVandenbilcke <romain.vandenbilcke@gmail.Com>
2024-09-24 11:02:35 +0200

    authentification

---
+++ views/Authentification.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="fr">>
+<head>
+    <meta charset="UTF-8">
+    <title>Authentification</title>
+
+    
+    
+</head>
+
+        </html>
+   
+    </header>
+    <hr />
+   
+    </nav>
+    <hr />
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+            <form method="POST" action=""
+             id="formulaireAuthentification">
+                <fieldset class="the-fieldset">
+                    <legend>Authentification</legend>
+                    <div class="row">
+                        <div class="col-2">
+                            <label for="adresse mail"> Adresse mail : </label>
+                        </div>
+                        <div class="col-3">
+                            <input type="mail" name="adresse mail" id="adresse mail" placeholder="Adresse mail?">
+                        </div>
+                    </div><br>
+                    <div class="row">
+                        <div class="col-2">
+                            <label for="mdp">Mot de passe :</label>
+                        </div>
+                        <div class="col-3">
+                            <input type="password" name="mdp"  id="mdp" placeholder="Mot de passe ?">
+                        </div>
+                    </div><br>
+            
+                    <input type="reset" value="Réinitialiser">
+                    <input type="submit" value="Valider" id="btSubmit">
+                    <br>
+                </fieldset>
+            </form>
+            <label id="lblMessage"></label>
+            </div>
+            <div class="col-6">
+
