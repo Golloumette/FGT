0c0f2d11b9ffec5ff611e14a99230d45b7ba9259
Alexis-Dutoit <dev.alexis14@gmail.com>
2024-09-25 16:52:17 +0200

    mercredi

--- views/interview.php
+++ views/interview.php
@@ -1,45 +1,56 @@
 <?php
 include '../controllers/interviewCTRL.php'
-?>
+    ?>
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
-    <title>Interview</title>  
+    <title>Interview</title>
+    <link href="../css/interview.css" rel="stylesheet" />
 </head>
+
 <body>
-<div style="display: flex; gap: 10px; align-items: center;">
-    <label for="user_prenom">Prénom :</label>
-    <p style="margin: 0;"><?php echo $listStudent[0]['prenom']; ?></p>
-</div>
 
-<div style="display: flex; gap: 10px; align-items: center;">
-    <label for="user_age">Age :</label>
-    <p style="margin: 0;"><?php echo $listStudent[0]['age']; ?></p>
-</div>
-<div style="display: flex; gap: 10px; align-items: center;">
-    <label for="user_prenom">Cursus :</label>
-    <p style="margin: 0;"><?php echo $listStudent[0]['nom_cursus']; ?></p>
-</div>
 
-<div style="display: flex; gap: 10px; align-items: center;">
-    <label for="user_age">Ecole :</label>
-    <p style="margin: 0;"><?php echo $listStudent[0]['nom']; ?></p>
-</div>
-<div style="display: flex; gap: 10px; align-items: center;">
-    <label for="user_prenom">Campus :</label>
-    <p style="margin: 0;"><?php echo $listStudent[0]['ville_campus']; ?></p>
-</div>
 
-<div style="display: flex; gap: 10px; align-items: center;">
-    <label for="user_age">Talent :</label>
-    <p style="margin: 0;"><?php echo $listStudent[0]['talent']; ?></p>
-</div>
-<img src="../images/portrait/<?php echo $listStudent[0]['photo']?>" height="150px"> 
+    <div class="gauche">
+        <img src="../images/portrait/<?php echo $listStudent[0]['photo'] ?>">
+
+
+        <div style="display: flex; gap: 10px; align-items: center;">
+            <label for="user_prenom">Prénom :</label>
+            <p style="margin: 0;"><?php echo $listStudent[0]['prenom']; ?></p>
+        </div>
+
+        <div style="display: flex; gap: 10px; align-items: center;">
+            <label for="user_age">Age :</label>
+            <p style="margin: 0;"><?php echo $listStudent[0]['age']; ?></p>
+        </div>
+        <div style="display: flex; gap: 10px; align-items: center;">
+            <label for="user_prenom">Cursus :</label>
+            <p style="margin: 0;"><?php echo $listStudent[0]['nom_cursus']; ?></p>
+        </div>
+
+        <div style="display: flex; gap: 10px; align-items: center;">
+            <label for="user_age">Ecole :</label>
+            <p style="margin: 0;"><?php echo $listStudent[0]['nom']; ?></p>
+        </div>
+
+        <div style="display: flex; gap: 10px; align-items: center;">
+            <label for="user_prenom">Campus :</label>
+            <p style="margin: 0;"><?php echo $listStudent[0]['ville_campus']; ?></p>
+        </div>
+
+    </div>
+
+
+    <div class="droite" style="display: flex; gap: 10px; align-items: center; ">
+        <label for="user_age">Talent :</label>
+        <p style="margin: 0;"><?php echo $listStudent[0]['talent']; ?></p>
+    </div>
 
 
 </body>
 
 </html>
-   
-  
