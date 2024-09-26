95370f095214b942c25ea7d7a38dda038e30a51a
carol <caroline.boucau59@gmail.com>
2024-09-26 10:51:42 +0200

    sql comment

--- controllers/interviewCTRL.php
+++ controllers/interviewCTRL.php
@@ -6,7 +6,8 @@ require_once '../daos/listInterviewDAO.php';
 // Initialiser la classe Connexion
 $connexion = new Connexion();
 $pdo = $connexion->seConnecter("../config/workshop.ini");
+// Appel de la fonction du DAO
 $listStudent = lastInsert($pdo);
-//var_dump($listStudent);
+// Encodage de la liste en JSON
 $jsonData = json_encode($listStudent);
 
--- daos/listInterviewDAO.php
+++ daos/listInterviewDAO.php
@@ -1,10 +1,12 @@
 <?php
-
+/*
+* function pour récupérer les 5 derniers enregistrement et renvoie un tableau
+*/
 
 function lastInsert (PDO $pdo) :array{
         $listStudent = array();
         try{
-         //  $sql="SELECT *FROM studenttalent ORDER BY id DESC LIMIT 5";
+               //la requête 
              $sql2="SELECT DISTINCT studenttalent.prenom,
              studenttalent.age,
              ecole.nom,
@@ -15,7 +17,7 @@ function lastInsert (PDO $pdo) :array{
               FROM studenttalent 
              INNER JOIN cursus ON studenttalent.cursus_ID = cursus.id
              INNER JOIN campus_ville ON studenttalent.campus_id =campus_ville.ID
-             INNER JOIN ecole ON studenttalent.cursus_ID = cursus.ID ORDER BY studenttalent.id DESC LIMIT 5 ";
+             INNER JOIN ecole ON studenttalent.ecole_ID = ecole.ID ORDER BY studenttalent.id DESC LIMIT 5 ";
             $cursor = $pdo->query($sql2);
             $listStudent = $cursor->fetchAll(PDO::FETCH_ASSOC);
 
